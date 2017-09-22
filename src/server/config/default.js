3e81ffb6d751e1d7a198e266b5907012cae7073b
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2017-09-22 15:01:24 +0200

    more simplified

--- src/server/config/default.js
+++ src/server/config/default.js
@@ -10,7 +10,7 @@ const cwd = process.cwd();
 export default {
   // name of the application, used in the `.ejs` template and by default in
   // the `platform` service to populate its view
-  appName: 'Grainfield',
+  appName: 'GrainField',
 
   // name of the environnement ('production' enable cache in express application)
   env: 'development',
